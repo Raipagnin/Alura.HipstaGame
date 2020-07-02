aae3620af05b87764d508b6fc76722e84d1851e0
Rai Pagin <64612453+Raipagnin@users.noreply.github.com>
2020-07-02 10:15:31 +0000

    Add files via upload

---
+++ scripts/load.js
@@ -0,0 +1,18 @@
+function preload(){
+  imagemCenario = loadImage("./assets/imagens/cenario/floresta.png");
+  imagemGameOver = loadImage("./assets/imagens/assets/game-over.png");
+  imagemPersonagens = loadImage("./assets/imagens/personagem/correndo.png");
+  
+  imagemInimigo = loadImage("./assets/imagens/inimigos/gotinha.png");
+  imagemInimigoGrande = loadImage("./assets/imagens/inimigos/troll.png");
+  imagemInimigoVoador = loadImage("./assets/imagens/inimigos/gotinha-voadora.png");
+
+  imagemTelaInicial = loadImage("./assets/imagens/assets/telaInicial.png");
+  imagemVida = loadImage("./assets/imagens/assets/coracao.png");
+  
+  fonteTelaInicial = loadFont("./assets/imagens/assets/fonteTelaInicial.otf");
+  fita = loadJSON('./fita/fita.json');
+  
+  somDoJogo = loadSound("./assets/sons/trilha_jogo.mp3");
+  somDoPulo = loadSound("./assets/sons/somPulo.mp3");
+}
